1477080ee2bb662b430112fbd0c060f6fe279e75
Jeremy Whitlock <jwhitlock@apache.org>
2026-08-27 11:28:33 -0400

    m8c 2.2.3 (new formula)

---
+++ Formula/m8c.rb
@@ -0,0 +1,19 @@
+class M8c < Formula
+  desc "Remote display client for the Dirtywave M8 Tracker"
+  homepage "https://github.com/laamaa/m8c"
+  url "https://github.com/laamaa/m8c/archive/refs/tags/v2.2.3.tar.gz"
+  sha256 "1fa5773a376e7b7ed5c65dcda5126d8e9933b1dbbaa95019e1afb025f5b6977a"
+  license all_of: ["MIT", "CC-BY-SA-3.0", "Zlib"]
+
+  depends_on "pkgconf" => :build
+  depends_on "libserialport"
+  depends_on "sdl3"
+
+  def install
+    system "make", "CC=#{ENV.cc}", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    system bin/"m8c", "--list"
+  end
+end
